e9a4feb346a80f1707a7c0b2577f888d411953bf
juhyeong <juhyeongee@gmail.com>
2022-03-02 21:07:52 +0900

    feat : add Routes & Link

---
+++ src/components/ClassDetails.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const ClassDetails = () => {
+  return <div>pages</div>;
+};
+
+export default ClassDetails;
---
+++ src/components/Home.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import styled from "styled-components";
+import backPic from "../images/img2.png";
+import cardPic from "../images/target.jpeg";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const Top = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 500px;
+  width: 100%;
+  background-color: wheat;
+`;
+
+const TopInner = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const TopTitle = styled.div`
+  font-size: 4vh;
+`;
+
+const Bottom = styled.div`
+  padding-left: 20vh;
+  padding-right: 20vh;
+`;
+const MidTitle = styled.div`
+  font-size: 4vh;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+const TopPhoto = styled.img``;
+
+const Card = styled.div`
+  height: 300px;
+  width: 180px;
+  border: 3px solid grey;
+  background-image: url("./images/target.jpeg");
+  border-radius: 10px;
+  margin-left: 30px;
+`;
+
+const CardHorder = styled.div`
+  display: flex;
+`;
+
+const Banner = styled.div`
+  margin-top: 50px;
+  width: 1000px;
+  height: 100px;
+  background-color: tomato;
+`;
+
+const Home = () => (
+  <>
+    <Top>
+      <TopInner>
+        <TopTitle>개발은 코멘투와 함께</TopTitle>
+        <TopPhoto src={backPic} />
+      </TopInner>
+    </Top>
+    <Bottom>
+      <MidTitle>인기 부트 캠프</MidTitle>
+      <CardHorder>
+        <Card>
+          <Link to="./ClassDetail">Look Details</Link>
+        </Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </CardHorder>
+      <MidTitle>특별 할인 캠프</MidTitle>
+      <CardHorder>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </CardHorder>
+      <Banner></Banner>
+    </Bottom>
+  </>
+);
+
+export default Home;
